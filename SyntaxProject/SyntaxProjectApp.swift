f6bab8636a31116be148212045bfbefef8e50cae
wellsfletcher <wellsfletcher@gmail.com>
2022-02-08 17:49:56 -0500

    Initial Commit

---
+++ SyntaxProject/SyntaxProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyntaxProjectApp.swift
+//  SyntaxProject
+//
+//  Created by Fletcher Wells on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct SyntaxProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
